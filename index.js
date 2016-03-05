6205068c2230ee60e0b5eb269b70db5f329fbc39
Lsong <song940@gmail.com>
2016-03-06 00:38:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
+'use strict';
+/**
+ * [STATUS_CODES description]
+ * @type {Object}
+ */
+const STATUS_CODES = {
+  2: 32,
+  3: 36,
+  4: 33,
+  5: 31
+};
+/**
+ * [color description]
+ * @param  {[type]} str [description]
+ * @param  {[type]} c   [description]
+ * @return {[type]}     [description]
+ */
+function color(str, c){
+  return "\x1b[" + c + "m" + str + "\x1b[0m";
+};
+
 // logger
 module.exports = function(req, res, next){
-  var start = new Date;
-  var err = null;
+  var err    = null;
+  var end    = res.end;
+  var start  = new Date;
+  var respod = false;
+  /**
+   * [function description]
+   * @return {[type]} [description]
+   */
+  res.end = function(){
+    end.apply(res, arguments);
+    respod = true;
+  };
   try{
     next();
   }catch(e){
     err = e;
-  } finally {
-    function color(str, c){
-      return "\x1b[" + c + "m" + str + "\x1b[0m";
-    };
-    if(err){
-      res.writeHead(500);
-    }
-    var cc = ({
-      2: 32,
-      3: 36,
-      4: 33,
-      5: 31
-    })[parseInt(res.statusCode / 100)];
-    console.log('%s %s %s %s %s',
-      err ? '-x' : '->',
-      color(req.method              , 35),
-      color(req.url                 , 90),
-      color(res.statusCode          , cc),
-      color((new Date - start)+ "ms", 90)
-    );
-    if(err){
-      throw err;
-    }
+  }
+  if(!respod && err) res.statusCode = 500;
+  var cc = STATUS_CODES[ res.statusCode / 100 | 0 ];
+  console.log(
+    err && !respod ? '-x' : '->'  ,
+    color(req.method              , 35),
+    color(req.url                 , 90),
+    color(res.statusCode          , cc),
+    color((new Date - start)+ "ms", 90)
+  );
+  if(err){
+    throw err;
   }
 };
